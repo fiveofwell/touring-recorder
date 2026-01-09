f2fdcc95ffe126bde7170aa4ac0367a727bc124c
fiveofwell <fiveofwell777@gmail.com>
2026-01-09 17:16:37 +0900

    rollback db session on exception

--- server/db.py
+++ server/db.py
@@ -5,5 +5,11 @@ engine = create_engine("sqlite:///./app.db", echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def get_session():
-    return Session(engine)
+def get_session() -> Session:
+    with Session(engine) as session:
+        try:
+            yield session
+        except Exception:
+            session.rollback()
+            raise
+
