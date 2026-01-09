cb0e9ed3274c73c7397446036849aeb557e495b5
fiveofwell <fiveofwell777@gmail.com>
2026-01-09 19:43:02 +0900

    update request format to support client-generated tour_id

--- client/api_client.py
+++ client/api_client.py
@@ -4,32 +4,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def start_tour():
-    try:
-        headers = {'X-API-Key': settings.X_API_KEY}
-        response = requests.get(settings.TOUR_START_URL, headers=headers, timeout=settings.POST_TIMEOUT_SEC)
-
-        if response.status_code == 200:
-            tour_id = response.json()["tour_id"]
-            logger.info("ツーリングID発行")
-            return tour_id 
-
-        logger.warning("ツーリングID発行失敗:%d", response.status_code)
-        return None
-
-    except requests.exceptions.RequestException:
-        logger.warning("ツーリングID発行失敗")
-        return None
-
-    except Exception:
-        logger.exception("ハンドルされていない例外")
-        return None
-
-
 def post_gps_data(tour_id, points):
     headers = {'X-API-Key': settings.X_API_KEY}
     payload = {
-        "tour_id": tour_id,
+        "device_id": settings.device_id,
         "points": points,
     }
 
@@ -42,7 +20,7 @@ def post_gps_data(tour_id, points):
         )
 
         response.raise_for_status()
-        return response.json()["accepted_ids"]
+        return response.json()["ok"]
 
     except requests.exceptions.HTTPError as e:
         if e.response is not None:
