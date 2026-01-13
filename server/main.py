80dc59a722fa562dad5861b02d43a9544228ecca
fiveofwell <fiveofwell777@gmail.com>
2026-01-14 00:17:24 +0900

    enable CORS for frontend

--- server/main.py
+++ server/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from services.exceptions import UnauthorizedKey, TourNotFound
 from db import create_db_and_tables
+import settings
 
 from routers.api_router import router as api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+origins = [
+    settings.FRONTEND_ORIGIN,
+]
+
+headers = [
+        "Content-Type",
+        "Authorization",
+        "X-API-KEY",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=headers,
+)
 
 @app.exception_handler(UnauthorizedKey)
 async def unauthorized_key_handler(
--- server/settings.py
+++ server/settings.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_KEY=os.getenv('API_KEY')
+API_KEY = os.getenv('API_KEY')
+FRONTEND_ORIGIN = os.getenv('FRONTEND_ORIGIN')
 
 if API_KEY is None:
     raise RuntimeError()
