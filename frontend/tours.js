83ad0789d5135f2891e0836a1323a2dc4a651314
fiveofwell <fiveofwell777@gmail.com>
2026-01-17 21:53:06 +0900

    add delete button for tours

--- frontend/tours.js
+++ frontend/tours.js
@@ -47,7 +47,7 @@ function showTours(tours) {
 	for (const tour of tours) {
 		const li = document.createElement('li');
 		li.innerHTML = `
-			<a href="./view-tour.html?tour_id=${encodeURIComponent(tour.tour_id)}">${tour.tour_id}</a>
+			<a class="tour-id" href="./view-tour.html?tour_id=${encodeURIComponent(tour.tour_id)}">${tour.tour_id}</a>
 			<ul>
 				<li>
 					<p>device id: ${tour.device_id}</p>
@@ -59,10 +59,45 @@ function showTours(tours) {
 					<p>Last seen at: ${formatDate(tour.last_seen_at)}</p>
 				</li>
 			</ul>
-		`
+		`;
+
+		const button = document.createElement("button");
+		button.textContent = "このツアーの履歴を削除";
+		button.addEventListener("click", () => {
+			deleteTour(tour.tour_id);
+		});
+
+		li.querySelector(".tour-id").after(" ", button);
 
 		list.appendChild(li)
 	}
+}
+
+
+async function deleteTour(tour_id) {
+	if (!confirm("本当にこのツアーの履歴を削除しますか？")) {
+		return;
+	}
+
+	const url = `${API_BASE_URL}/tours/${tour_id}`;
+
+	try {
+		const response = await fetch(
+			url, {
+				method: "DELETE",
+			}
+		);
+	
+		if (!response.ok) {
+			throw new Error(`HTTP ${response.status} ${response.statusText}`);
+		}
+
+		location.reload();
+	} catch (error) {
+		console.error('エラーが発生しました', error);
+		showError(error);
+	}
+	return;
 }
 
 
