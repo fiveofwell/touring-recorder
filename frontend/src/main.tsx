a09df657173e088ba5f9c8f1592ff0dfa66ef767
fiveofwell <fiveofwell777@gmail.com>
2026-03-27 00:18:52 +0900

    migrate from static HTML to React + Vite

---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { TourListPage } from './tour-list-page';
+import { TourDetailPage } from './tour-detail-page';
+import { TourNameChangePage } from './tour-name-change-page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TourListPage />} />
+	<Route path='/tours/:tour_id' element={<TourDetailPage />} />
+	<Route path='/tours/:tour_id/name-change' element={<TourNameChangePage />} />
+
+	
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ frontend/src/tour-detail-page.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import { TourMap } from './components/map';
+import type { Point } from './components/types';
+
+const parsePoint = (raw: any): Point => ({
+	...raw,
+	timestamp: new Date(raw.timestamp + "Z"),
+})
+
+export const TourDetailPage = () => {
+	const { tour_id } = useParams<{ tour_id: string }>()
+
+	const [points, setPoints] = useState<Point[]>([])
+	const [loading, setLoading] = useState(true)
+	const [failed, setFailed] = useState(false)
+
+	useEffect(() => {
+		fetch(`/api/internal/tours/${tour_id}`)
+		.then(response => {
+			if (!response.ok){
+				throw new Error('APIエラー')
+			}
+			return response.json()
+		})
+		.then(data => {
+			setPoints(data.points.map((p: any) => parsePoint(p)))
+		})
+		.catch(error => {
+			console.error(error)
+			setFailed(true)
+		})
+		.finally(() => setLoading(false))
+	}, [])
+
+	if (loading) return ( <p>Loading...</p> )
+	if (failed) return ( <p>エラーが発生しました</p> )
+
+	return (
+		<TourMap
+			points={points}
+		/>
+	)
+
+}
